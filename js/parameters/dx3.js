209e6a80458866407bde3cd80a941430f23372dc
nekomaru7575-hub <nekomaru7575@gmail.com>
2026-07-23 14:49:47 +0900

    微修正

--- js/parameters/dx3.js
+++ js/parameters/dx3.js
@@ -2,8 +2,8 @@ import { buildParameters } from './paramFactory.js';
 
 export const DX3_PARAMETERS =[
     {key : "corruption", label : "侵蝕率",value : 0},
-    {key : "corDB", label : "侵蝕率ダイスボーナス",value : 0 , editable : false},
-    {key : "corEB", label : "侵蝕率エフェクトボーナス",value : 0, editable : false}
+    {key : "corDB", label : "侵蝕率ダイスボーナス",value : 0 , editable : false,visible : false},
+    {key : "corEB", label : "侵蝕率エフェクトボーナス",value : 0, editable : false,visible : false}
 ]
 
 export function buildDX3Parameters(){
